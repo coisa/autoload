01175719d80d0f4179e1a7846d396f853508e209
Felipe Sayão Lobato Abreu <github@felipeabreu.com.br>
2020-04-15 21:41:38 -0300

    Added PHPDoc to interface

--- src/AutoloaderInterface.php
+++ src/AutoloaderInterface.php
@@ -13,11 +13,31 @@
 
 namespace CoiSA\Autoload;
 
+/**
+ * Interface AutoloaderInterface
+ *
+ * @package CoiSA\Autoload
+ */
 interface AutoloaderInterface
 {
+    /**
+     * Loads the given class or interface.
+     *
+     * @param string $class The name of the class
+     *
+     * @return null|bool True, if loaded
+     */
     public function loadClass($class);
 
+    /**
+     * Registers this instance as an autoloader.
+     *
+     * @param bool $prepend Whether to prepend the autoloader or not
+     */
     public function register($prepend = false);
 
+    /**
+     * Unregisters this instance as an autoloader.
+     */
     public function unregister();
 }
