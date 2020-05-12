500ad17f1bdbe0c3f7dd90dbc7e258c4c69c8dae
Felipe Sayão Lobato Abreu <github@felipeabreu.com.br>
2020-05-12 16:58:54 -0300

    WIP: Tests

--- tests/AutoloaderTest.php
+++ tests/AutoloaderTest.php
@@ -11,15 +11,39 @@
  * @license   https://opensource.org/licenses/MIT MIT License
  */
 
-namespace CoiSA\Autoload;
+namespace Test;
 
+use CoiSA\Autoload\Autoloader;
+use Composer\Autoload\ClassLoader;
 use PHPUnit\Framework\TestCase;
+use Prophecy\Prophecy\ObjectProphecy;
+use Psr\Log\LoggerInterface;
 
 /**
  * Class AutoloaderTest
  *
- * @package CoiSA\Autoload
+ * @package Test
  */
 final class AutoloaderTest extends TestCase
 {
+    /** @var ClassLoader|ObjectProphecy */
+    private $classLoader;
+
+    /** @var LoggerInterface|ObjectProphecy */
+    private $logger;
+
+    /** @var Autoloader */
+    private $autoloader;
+
+    public function setUp(): void
+    {
+        $this->classLoader = $this->prophesize(ClassLoader::class);
+        $this->logger      = $this->prophesize(LoggerInterface::class);
+
+        $this->autoloader = new Autoloader(
+            $this->classLoader->reveal(),
+            \sys_get_temp_dir() . '/' . \uniqid('autoload_classmap', false),
+            $this->logger->reveal()
+        );
+    }
 }
