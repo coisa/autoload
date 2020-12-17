2b5f4fc1a6abeb36aca3a1fd9e786608560060bc
Felipe Sayão Lobato Abreu <github@felipeabreu.com.br>
2020-12-17 16:00:49 -0300

    PHP CS-Fix

--- src/Exception/RuntimeException.php
+++ src/Exception/RuntimeException.php
@@ -1,5 +1,17 @@
 <?php
 
+/**
+ * This file is part of coisa/autoload.
+ *
+ * This source file is subject to the license that is bundled
+ * with this source code in the file LICENSE.
+ *
+ * @link      https://github.com/coisa/autoload
+ *
+ * @copyright Copyright (c) 2020 Felipe Sayão Lobato Abreu <github@felipeabreu.com.br>
+ * @license   https://opensource.org/licenses/MIT MIT License
+ */
+
 namespace CoiSA\Autoload\Exception;
 
 /**
